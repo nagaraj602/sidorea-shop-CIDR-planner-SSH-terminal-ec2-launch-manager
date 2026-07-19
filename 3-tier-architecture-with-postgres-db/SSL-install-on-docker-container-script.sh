dc41564ddb93b3b65e11c6012c21e37cf93c92b3
nagaraj602 <264412729+nagaraj602@users.noreply.github.com>
2026-07-19 18:37:04 +0530

    Update SSL-install-on-docker-container-script.sh

--- 3-tier-architecture-with-postgres-db/SSL-install-on-docker-container-script.sh
+++ 3-tier-architecture-with-postgres-db/SSL-install-on-docker-container-script.sh
@@ -39,7 +39,7 @@ server {
 
     # Route Frontend traffic to the Frontend Docker Container
     location / {
-        proxy_pass http://127.0.0.1:8080;
+        proxy_pass http://127.0.0.1:8085;
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
@@ -126,7 +126,7 @@ server {
     server_name $DOMAIN www.$DOMAIN;
 
     location / {
-        proxy_pass http://127.0.0.1:8080;
+        proxy_pass http://127.0.0.1:8085;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
